20ac6554d537d7156de27907f4ee99e173f80d6f
behzadkeimasi <61060857+behzadkeimasi@users.noreply.github.com>
2022-08-06 02:43:52 +0430

    Add files via upload

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+m './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="body">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+     
